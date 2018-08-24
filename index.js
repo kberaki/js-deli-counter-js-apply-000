a440444f386cf734275624afb2776174b69ebcac
kesete beraki <berakik@gmail.com>
2018-08-24 16:55:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var name = [];
 function takeANumber(katzDeliLine, name){
 katzDeliLine.push(`${name}`);
 
-return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
-//return ("welcome, "  name[]+ ". " + "You are number "  katzDeliLine.length[] + " in line.");
+//return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
+return (`The ${katzDeliLine.length}` line, belongs to: $[name]);
  }
 
 
